2177d48b502780bb3bcf8fed3bff21ca767ddc6a
Wallace Neto <wallace.neto@outlook.com>
2023-12-31 15:31:48 -0300

    feat: creatign users flatlist

---
+++ src/types/userType.tsx
@@ -0,0 +1,8 @@
+type User = {
+  id: number;
+  name: string;
+  email: string;
+  avatarUrl: string;
+};
+
+export default User;
--- src/views/UserList.tsx
+++ src/views/UserList.tsx
@@ -1,6 +1,32 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {FlatList, View} from 'react-native';
 
-export default function UserList() {
-  return <Text>User List</Text>;
+import users from '../data/users';
+import User from '../types/userType';
+import {Avatar, ListItem} from 'react-native-elements';
+
+export default function UserList(props: any) {
+  function getUserItem({item}: {item: User}) {
+    return (
+      <ListItem
+        bottomDivider
+        onPress={() => props.navigation.navigate('UserForm')}>
+        <Avatar source={{uri: item.avatarUrl}} />
+        <ListItem.Content>
+          <ListItem.Title>{item.name}</ListItem.Title>
+          <ListItem.Subtitle>{item.email}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    );
+  }
+
+  return (
+    <View>
+      <FlatList
+        data={users}
+        keyExtractor={user => user.id.toString()}
+        renderItem={getUserItem}
+      />
+    </View>
+  );
 }
