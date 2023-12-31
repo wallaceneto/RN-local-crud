245ceffc03982bd261325938ca59db143f5afdf7
Wallace Neto <wallace.neto@outlook.com>
2023-12-31 15:46:57 -0300

    feat: adding users action buttons

--- src/views/UserList.tsx
+++ src/views/UserList.tsx
@@ -1,21 +1,43 @@
 import React from 'react';
-import {FlatList, View} from 'react-native';
+import {Alert, FlatList, View} from 'react-native';
 
 import users from '../data/users';
 import User from '../types/userType';
-import {Avatar, ListItem} from 'react-native-elements';
+import {Avatar, Button, Icon, ListItem} from 'react-native-elements';
 
 export default function UserList(props: any) {
+  function confirmUserDeletion(item: User) {
+    Alert.alert('Excluir Usuário', 'Deseja mesmo excluir esse usuário?', [
+      {
+        text: 'sim',
+        onPress() {
+          console.warn('delete usuer ' + item.id);
+        },
+      },
+      {
+        text: 'não',
+      },
+    ]);
+  }
+
   function getUserItem({item}: {item: User}) {
     return (
-      <ListItem
-        bottomDivider
-        onPress={() => props.navigation.navigate('UserForm')}>
+      <ListItem bottomDivider>
         <Avatar source={{uri: item.avatarUrl}} />
         <ListItem.Content>
           <ListItem.Title>{item.name}</ListItem.Title>
           <ListItem.Subtitle>{item.email}</ListItem.Subtitle>
         </ListItem.Content>
+        <Button
+          type="clear"
+          icon={<Icon name="edit" size={25} color="orange" />}
+          onPress={() => props.navigation.navigate('UserForm')}
+        />
+        <Button
+          type="clear"
+          icon={<Icon name="delete" size={25} color="red" />}
+          onPress={() => confirmUserDeletion(item)}
+        />
       </ListItem>
     );
   }
